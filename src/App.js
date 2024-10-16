48767946936f25c37e85877a9e32c858236cfddf
jiya <jiyajoy123@gmail.com>
2024-10-16 12:08:47 -0400

    post review

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css';
+import Home from './component/Home/Home'
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
